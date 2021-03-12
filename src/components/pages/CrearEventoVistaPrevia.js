b32882cb50413146366b28bd9bd309838878c572
CarlosAndresQuel00 <and.quel5@gmail.com>
2021-03-11 19:16:52 -0500

    Updated component

--- src/components/pages/CrearEventoVistaPrevia.js
+++ src/components/pages/CrearEventoVistaPrevia.js
@@ -1,130 +1,173 @@
-import React from 'react'
-import "../../index.css"
-import {Row, Col, Image, Button, Upload, message, Mentions} from "antd";
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import React from "react";
+import "../../index.css";
+import { Row, Col, Image, Button, Upload, message, Input } from "antd";
+import { LoadingOutlined, CloseOutlined } from "@ant-design/icons";
+
+const { TextArea } = Input;
 
 export default function CrearEventoVistaPrevia(props) {
-    const state = {
-        loading: false,
-    };
-    const style = {display: "inline-block"};
-    const { loading, imageUrl } = state;
-    const uploadButton = (
-        <div>
-            {loading ? <LoadingOutlined /> : <PlusOutlined />}
-            <div style={{ marginTop: 8 }}>Upload</div>
-        </div>
-    );
+  const [state, setState] = React.useState(false);
 
-    function getBase64(img, callback) {
-        const reader = new FileReader();
-        reader.addEventListener('load', () => callback(reader.result));
-        reader.readAsDataURL(img);
+  const style = {
+    display: "inline-block",
+    padding: "10px 20px 0",
+  };
+  const style1 = {
+    width: "180px",
+    height: "180px",
+    fontSize: "250px",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    background: "#E0E6ED",
+    transform: "translateY(20px)",
+  };
+  const style2 = {
+    display: "inline-block",
+    padding: "0 130px 30px",
+  };
+  const styleImage = {
+    backgroundImage:
+      "url(https://essaeformacion.com/storage/gcs/common_fields/posts/201/K7IeYEBz2gxajLrXLfycSK1ymGIwiP0BpvUANChrQzCVDWLETeQv8TaHVE9D.jpg)",
+    height: "150px",
+  };
+  const stylePadding = {
+    paddingBottom: "100px",
+  };
+  const styleNewPadding = {
+    paddingBottom: "30px",
+  };
+
+  const { loading, imageUrl } = state;
+  const uploadButton = (
+    <div style={style1}>
+      {loading ? <LoadingOutlined /> : <CloseOutlined />}
+    </div>
+  );
+
+  function getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => callback(reader.result));
+    reader.readAsDataURL(img);
+  }
+
+  const handleChange = (info) => {
+    if (info.file.status === "uploading") {
+      setState({ loading: true });
+      return;
     }
-
-    const handleChange = (info) => {
-        if (info.file.status === 'uploading') {
-            this.setState({loading: true});
-            return;
-        }
-        if (info.file.status === 'done') {
-            // Get this url from response in real world.
-            getBase64(info.file.originFileObj, imageUrl =>
-                this.setState({
-                    imageUrl,
-                    loading: false,
-                }),
-            );
-        }
-    };
-
-    function beforeUpload(file) {
-        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-        if (!isJpgOrPng) {
-            message.error('You can only upload JPG/PNG file!');
-        }
-        const isLt2M = file.size / 1024 / 1024 < 2;
-        if (!isLt2M) {
-            message.error('Image must smaller than 2MB!');
-        }
-        return isJpgOrPng && isLt2M;
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (imageUrl) =>
+        setState({
+          imageUrl,
+          loading: false,
+        })
+      );
     }
+  };
 
-    return (
-        <div align="middle">
-            <div>
-                <h1>
-                    {" "}
-                    Widinson
-                </h1>
+  function beforeUpload(file) {
+    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+    if (!isJpgOrPng) {
+      message.error("Puedes subir solamente arechivos JPG/PNG!");
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error("Tamaño de la imagen menor a 2MB!");
+    }
+    return isJpgOrPng && isLt2M;
+  }
 
-                    <Image
-                        src="https://png.pngtree.com/png-clipart/20190516/original/pngtree-users-vector-icon-png-image_3725294.jpg"
-                        width={250}
-                        height={250}
-                        preview={false}
-                    />
+  return (
+    <div align="middle">
+      <Row>
+        <Col>
+          <div style={stylePadding}>
+            <div style={styleImage}>
+              <h1> Widinson</h1>
 
-                <h2>
-                    {" "}
-                    Vista Previa
-                </h2>
+              <Image
+                src="https://png.pngtree.com/png-clipart/20190516/original/pngtree-users-vector-icon-png-image_3725294.jpg"
+                width={150}
+                height={150}
+                preview={false}
+                style={{ borderRadius: "50%" }}
+              />
+
+              <h2> Vista Previa</h2>
             </div>
+          </div>
+        </Col>
+      </Row>
 
-            <Row>
-                <Col span={8} style={style}>
-                    <Row>
-                        <Upload
-                            name="avatar"
-                            listType="picture-card"
-                            className="avatar-uploader"
-                            showUploadList={false}
-                            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                            beforeUpload={beforeUpload}
-                            onChange={handleChange}
-                        >
-                            {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
-                        </Upload>
-                    </Row>
-                    <Row>
-                        <Button type="primary">
-                            Cancelar
-                        </Button>
-                    </Row>
+      <Row>
+        <Col>
+          <Row style={styleNewPadding}>
+            <Col span={8} style={style}>
+              <Upload
+                name="avatar"
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList={false}
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                beforeUpload={beforeUpload}
+                onChange={handleChange}
+              >
+                {imageUrl ? (
+                  <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+                ) : (
+                  uploadButton
+                )}
+              </Upload>
+            </Col>
+            <Col span={8} style={style}>
+              <h4>
+                Widinson
+                <br />
+                23 de Julio
+                <br />7 pm
+              </h4>
+              <TextArea
+                style={{ resize: "none" }}
+                rows={3}
+                placeholder="Ven y disfruta de un momnto inolvidable con Widinson."
+                disabled
+              />
+              <h4>
+                Entrada: 20 USD
+                <br />
+                Golden Pass: 100 USD
+              </h4>
+            </Col>
+            <Col span={8} style={style}>
+              <iframe
+                width="220"
+                height="145"
+                src="https://www.youtube.com/embed/tgbNymZ7vqY"
+              />
+              <h4>Video Promocional</h4>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Row>
+                <Button type="primary" danger>
+                  Publicar
+                </Button>
+              </Row>
+              <Row>
+                <Col span={12} style={style2}>
+                  <Button type="primary">Cancelar</Button>
                 </Col>
-                <Col span={8} style={style}>
-                    <Row>
-                        <h3>Coca Cola Zero</h3>
-                    </Row>
-                    <Row>
-                        <Mentions rows={3} disable>
-                            Cero calorías tomala sin cargo de conciencia.
-                        </Mentions>
-                    </Row>
-                    <Row>
-                        <h3>Alcance: 2000 personas</h3>
-                        <h3>Costo: 20 USD</h3>
-                    </Row>
-                    <Row>
-                        <Button type="primary" danger>
-                            Publicar
-                        </Button>
-                    </Row>
+                <Col span={12} style={style2}>
+                  <Button type="primary">Editar</Button>
                 </Col>
-                <Col span={8} style={style}>
-                    <Row>
-                        <iframe width="420" height="245" src="https://www.youtube.com/embed/tgbNymZ7vqY" />
-                    </Row>
-                    <Row>
-                        <h3>Video Promocional</h3>
-                    </Row>
-                    <Row>
-                        <Button type="primary">
-                            Editar
-                        </Button>
-                    </Row>
-                </Col>
-            </Row>
-            </div>
-    )
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
 }
